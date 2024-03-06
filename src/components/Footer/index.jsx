9e95ab01879013407e083df575bf3a3b09ad2d82
Gregory <gregorycedro@gmail.com>
2024-03-06 15:29:35 -0300

    add footer contact infos

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,21 @@
-import { Container } from "./styles"
+import { Container, Info, Copyright } from "./styles"
+import Logo from "../../assets/footer-logo.png"
+import { MdEmail } from "react-icons/md";
 
 export function Footer(){
   return(
     <Container>
-      <p>© 2024 Concreto Construções e Empreendimentos. Todos os direitos reservados.</p>
+      <Info>
+        <img src={Logo} alt="Logo da Concreto" />
+        <div>
+          <h1>Contato:</h1>
+          <p><strong>Email:</strong> concreto.tijucas@gmail.com</p>
+          <p><strong>Telefone:</strong> (48) 99699-0112</p>
+        </div>
+      </Info>
+      <Copyright>
+        <p>© 2024 Concreto Construções e Empreendimentos. Todos os direitos reservados.</p>
+      </Copyright>
     </Container>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,17 +2,49 @@ import styled from "styled-components";
 
 export const Container = styled.footer`
   grid-area: footer;
+  
   width: 100%;
   height: 100%;
-  padding: 10px 100px;
 
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Info = styled.div`
+  background: ${({theme}) => theme.COLORS.BLUE_100};
+  padding: 20px 200px;
+  width: 100%;
+
+  display: flex;
+
+  justify-content: center;
+  align-items: center;
+  gap: 150px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+
+  h1 {
+    font-size: 24px;
+    color: ${({theme})=> theme.COLORS.BLUE}
+  }
+
+  img {
+    width: 200px;
+  }
+`;
+
+export const Copyright = styled.div`
   background-color: ${({theme})=> theme.COLORS.BLUE};
-  
   display: flex;
   align-items: center;
   justify-content: center;
-  
 
+  padding: 10px;
+  
   p {
     color: ${({theme})=>theme.COLORS.WHITE}
   }
