5ab94be37671cd105d1b9b96699e983b9574fadb
Gregory <gregorycedro@gmail.com>
2024-03-06 23:53:09 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home'
-import { About } from './pages/About'
-import { Contact } from './pages/Contact'
+import { Routes } from './routes'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
@@ -11,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <About />
+      <Routes />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
