f7e58cea3432e0c6274d4f876eb17196a1e5225b
Gregory <gregorycedro@gmail.com>
2024-03-11 16:29:49 -0300

    add dashboard page

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,15 @@
+import { Container, Main } from './styles'
+import { Header } from '../../components/Header'
+import { Footer } from '../../components/Footer'
+
+export function Dashboard(){
+  return(
+    <Container>
+      <Header/>
+      <Main>
+
+      </Main>
+      <Footer/>
+    </Container>
+  )
+}
